22e5a80d342cbcf5b456e60cb194d2a7097018ac
anand-mcs <59893145+anand-mcs@users.noreply.github.com>
2020-01-25 14:29:24 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SingleTonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleTon obj1 = SingleTon.GetInstance;
+            obj1.Display("Object 1");
+            SingleTon obj2 = SingleTon.GetInstance;
+            obj2.Display("Object 2");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SingleTon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingleTonPattern
+{
+    public sealed class SingleTon
+    {
+        private static int counter;
+        //Changed the constructor to private to control the instantiation.
+        private static SingleTon Instance = null;
+
+        public static SingleTon GetInstance
+        {
+            get
+            {
+                if (Instance == null)
+                    Instance = new SingleTon();
+
+                return Instance;
+
+            }
+        }
+       
+        
+    private SingleTon()
+        {
+            counter++;
+            Console.WriteLine("Instantiated " + counter.ToString());
+    
+        }
+        public void Display(string Msg)
+        {
+            Console.WriteLine(Msg);
+        }
+    }
+}
